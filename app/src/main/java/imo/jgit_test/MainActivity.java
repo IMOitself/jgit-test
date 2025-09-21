cc8845998a27031497f36d2d93d88ccf82475f55
IMO <180146096+IMOitself@users.noreply.github.com>
2025-09-22 06:19:43 +0800

    feat: add storage permission and set button as the activity

--- app/src/main/java/imo/jgit_test/MainActivity.java
+++ app/src/main/java/imo/jgit_test/MainActivity.java
@@ -1,12 +1,51 @@
 package imo.jgit_test;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.Settings;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
+    Button button;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        if (! hasStoragePermission(this)) requestStoragePermission(this);
+
+        button = new Button(this);
+        button.setText("check Downloads/MyTestRepo");
+        button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+					// TODO: test jgit
+                }
+            });
+		setContentView(button);
+    }
+
+    public static boolean hasStoragePermission(Activity activity) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            return Environment.isExternalStorageManager();
+        } else {
+            return activity.checkSelfPermission(android.Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED;
+        }
+    }
+
+    public static void requestStoragePermission(Activity activity) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            Intent intent = new Intent(Settings.ACTION_MANAGE_APP_ALL_FILES_ACCESS_PERMISSION);
+            intent.setData(Uri.parse("package:" + activity.getPackageName()));
+            activity.startActivity(intent);
+        } else {
+            activity.requestPermissions(new String[]{android.Manifest.permission.READ_EXTERNAL_STORAGE}, 100);
+            activity.requestPermissions(new String[]{android.Manifest.permission.WRITE_EXTERNAL_STORAGE}, 100);
+        }
     }
 }
